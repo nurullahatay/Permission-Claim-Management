6ff71b33764e2cf085f44aedbf12a241d61d9ff6
Nurullah ATAY <natay78@gmail.com>
2018-09-03 21:43:37 +0300

    tarih hesaplama sorunu yapıldı

--- WebContent/js/permission.js
+++ WebContent/js/permission.js
@@ -503,10 +503,12 @@ function workingDaysBetweenDates(d0, d1) {
     }
     return days;
 }
-           
+
+
+
 function parseDate(input) {
 	// Transform date from text to date
-  var parts = input.match(/-(\d+)/g);
+  var parts = input.match(/(\d+)/g);
   // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
   return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
 }
